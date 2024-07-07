135b612abf8a2a90ebbff797d5192f1322b3c761
Akim Svoboda <akim.svoboda@gmail.com>
2024-07-08 02:36:43 +0300

    Create theme switch button

---
+++ lib/presentation/common_widgets/theme_switch_button.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:my_portfolio/app/providers/theme_provider.dart';
+
+class ThemeSwitchFAB extends ConsumerWidget {
+  const ThemeSwitchFAB({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final themeMode = ref.watch(themeProvider);
+
+    Color fabBackgroundColor = Theme.of(context).primaryColor;
+    if (themeMode == ThemeMode.dark) {
+      fabBackgroundColor = const Color.fromARGB(255, 39, 39, 39);
+    } else {
+      fabBackgroundColor = const Color.fromARGB(255, 240, 240, 240);
+    }
+
+    return FloatingActionButton(
+      mini: true,
+      backgroundColor: fabBackgroundColor,
+      shape: CircleBorder(
+        side: BorderSide(color: themeMode == ThemeMode.light ? Colors.black : Colors.white, width: 0.3),
+      ),
+      onPressed: () {
+        ref.read(themeProvider.notifier).state = themeMode == ThemeMode.light ? ThemeMode.dark : ThemeMode.light;
+      },
+      child: Icon(
+          color: themeMode == ThemeMode.light ? Colors.black : Colors.white,
+          themeMode == ThemeMode.light ? Icons.dark_mode_outlined : Icons.light_mode_outlined),
+    );
+  }
+}

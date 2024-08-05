72647d0d0aeb741421268b90f97b089226b7c398
Akim Svoboda <akim.svoboda@gmail.com>
2024-08-05 13:57:33 +0300

    Change text styles

--- lib/presentation/common_widgets/custom_app_bar.dart
+++ lib/presentation/common_widgets/custom_app_bar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:responsive_builder/responsive_builder.dart';
 
 class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
@@ -22,8 +23,14 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
 
   Widget _buildMobileAppBar() {
     return AppBar(
-      leading: const Padding(padding: EdgeInsets.all(10), child: Text('Akim Svoboda')),
+      leading: const LabelName(),
       leadingWidth: 150,
+      bottom: PreferredSize(
+          preferredSize: const Size.fromHeight(4.0),
+          child: Container(
+            color: Colors.grey[300],
+            height: 0.5,
+          )),
       actions: [
         IconButton(
           icon: const Icon(
@@ -39,46 +46,7 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   Widget _buildTabletAppBar() {
     return AppBar(
       centerTitle: true,
-      leading: const Padding(padding: EdgeInsets.all(10), child: Text('Akim Svoboda')),
-      leadingWidth: 150,
-      title: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          TextButton(
-            onPressed: () {},
-            child: const Text('Home'),
-          ),
-          TextButton(
-            onPressed: () {},
-            child: const Text('Profile'),
-          ),
-          TextButton(
-            onPressed: () {},
-            child: const Text('Settings'),
-          ),
-        ],
-      ),
-      actions: [
-        IconButton(
-          icon: const Icon(Icons.search),
-          onPressed: () {},
-        ),
-        IconButton(
-          icon: const Icon(Icons.notifications),
-          onPressed: () {},
-        ),
-        IconButton(
-          icon: const Icon(Icons.more_vert),
-          onPressed: () {},
-        ),
-      ],
-    );
-  }
-
-  Widget _buildDesktopAppBar() {
-    return AppBar(
-      centerTitle: true,
-      leading: const Padding(padding: EdgeInsets.all(10), child: Text('Akim Svoboda')),
+      leading: const LabelName(),
       leadingWidth: 150,
       bottom: PreferredSize(
           preferredSize: const Size.fromHeight(4.0),
@@ -118,5 +86,66 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
         ),
       ],
     );
+  }
+
+  Widget _buildDesktopAppBar() {
+    return AppBar(
+      centerTitle: true,
+      //leading: const Padding(padding: EdgeInsets.all(10), child: Text('Akim Svoboda')),
+      leading: const LabelName(),
+      leadingWidth: 150,
+      bottom: PreferredSize(
+          preferredSize: const Size.fromHeight(4.0),
+          child: Container(
+            color: Colors.grey[300],
+            height: 0.5,
+          )),
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TextButton(
+            onPressed: () {},
+            child: const Text('Home'),
+          ),
+          TextButton(
+            onPressed: () {},
+            child: const Text('Profile'),
+          ),
+          TextButton(
+            onPressed: () {},
+            child: const Text('Settings'),
+          ),
+        ],
+      ),
+      actions: [
+        IconButton(
+          icon: const Icon(Icons.search),
+          onPressed: () {},
+        ),
+        IconButton(
+          icon: const Icon(Icons.notifications),
+          onPressed: () {},
+        ),
+        IconButton(
+          icon: const Icon(Icons.more_vert),
+          onPressed: () {},
+        ),
+      ],
+    );
+  }
+}
+
+class LabelName extends StatelessWidget {
+  const LabelName({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Text(
+      'Akim Svoboda',
+      style: GoogleFonts.lato(fontSize: 18, fontWeight: FontWeight.bold),
+    ));
   }
 }
--- lib/presentation/common_widgets/title_text.dart
+++ lib/presentation/common_widgets/title_text.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import 'package:my_portfolio/app/providers/theme_provider.dart';
 import 'package:my_portfolio/app/utils/gradient_text.dart';
@@ -22,7 +23,7 @@ class TitleText extends ConsumerWidget {
       children: [
         GradientText(
           'Hi! I\'m Akim Svoboda!',
-          style: TextStyle(fontSize: titleSize),
+          style: GoogleFonts.lato(fontSize: titleSize, fontWeight: FontWeight.bold),
           gradient: LinearGradient(
             colors: themeMode == ThemeMode.light
                 ? [const Color.fromARGB(255, 99, 99, 99), const Color.fromARGB(255, 49, 49, 49)]
@@ -34,18 +35,18 @@ class TitleText extends ConsumerWidget {
         const SizedBox(height: 20),
         Text(
           'Flutter Developer | Mobile & Web Enthusiast',
-          style: TextStyle(
+          style: GoogleFonts.nunito(
             fontSize: helpTextSize,
-            color: Theme.of(context).colorScheme.secondary,
+            color: Theme.of(context).colorScheme.onSurface,
           ),
           textAlign: TextAlign.center,
         ),
         const SizedBox(height: 20),
         Text(
           'Passionate about creating beautiful and functional applications using Flutter.',
-          style: TextStyle(
+          style: GoogleFonts.nunito(
             fontSize: helpTextSize,
-            color: Theme.of(context).colorScheme.secondary,
+            color: Theme.of(context).colorScheme.onSurface,
           ),
           textAlign: TextAlign.center,
         ),

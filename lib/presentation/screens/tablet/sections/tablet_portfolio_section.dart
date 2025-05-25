a8e36f0f664749c7706fe5563652e901687dd9a8
Akim Svoboda <106164446+Akimskate@users.noreply.github.com>
2025-05-26 02:18:26 +0300

    Particle background (#11)

--- lib/presentation/screens/tablet/sections/tablet_portfolio_section.dart
+++ lib/presentation/screens/tablet/sections/tablet_portfolio_section.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:my_portfolio/presentation/common_widgets/section_wrapper.dart';
+import 'package:particles_network/particles_network.dart';
 
 class TabletPortfolioSection extends StatelessWidget {
   const TabletPortfolioSection({super.key, required this.scrollController});
@@ -12,20 +14,47 @@ class TabletPortfolioSection extends StatelessWidget {
       child: AnimatedBuilder(
         animation: scrollController,
         builder: (context, child) {
-          return Row(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: List.generate(
-              3,
-              (index) {
-                return Container(
-                  margin: const EdgeInsets.only(bottom: 24),
-                  height: 200,
-                  color: Colors.grey[300],
-                  child: Center(child: Text('Project ${index + 1}')),
-                );
-              },
-            ),
+          double offset = scrollController.hasClients ? scrollController.offset : 0;
+          double parallaxOffset = offset * 0.1;
+          return Stack(
+            children: [
+              Positioned.fill(
+                child: Transform.translate(
+                  offset: Offset(0, -parallaxOffset * 1),
+                  child: ParticleNetwork(
+                    touchActivation: true,
+                    particleCount: 40,
+                    maxSpeed: 1.5,
+                    maxSize: 1.5,
+                    lineDistance: 170,
+                    particleColor: Theme.of(context).brightness == Brightness.dark ? Colors.white : Colors.black,
+                    lineColor: Theme.of(context).brightness == Brightness.dark ? Colors.white54 : Colors.black26,
+                    touchColor: Theme.of(context).colorScheme.secondary,
+                  ),
+                ),
+              ),
+              SectionWrapper(
+                center: true,
+                child: Transform.translate(
+                  offset: Offset(0, -parallaxOffset),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: List.generate(
+                      3,
+                      (index) {
+                        return Container(
+                          margin: const EdgeInsets.all(16),
+                          height: 200,
+                          color: Colors.black,
+                          child: Center(child: Text('Project ${index + 1}')),
+                        );
+                      },
+                    ),
+                  ),
+                ),
+              ),
+            ],
           );
         },
       ),

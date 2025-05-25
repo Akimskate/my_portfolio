45bf6a93ebd94b69a4632bb01c886d5bd75dce51
Akim Svoboda <akim.svoboda@gmail.com>
2025-05-26 02:17:51 +0300

    Particle background

--- lib/presentation/screens/desktop/sections/desktop_portfolio_section.dart
+++ lib/presentation/screens/desktop/sections/desktop_portfolio_section.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:my_portfolio/presentation/common_widgets/section_wrapper.dart';
+import 'package:particles_network/particles_network.dart';
 
 class DesktopPortfolioSection extends StatelessWidget {
   const DesktopPortfolioSection({super.key, required this.scrollController});
@@ -7,34 +9,54 @@ class DesktopPortfolioSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(32),
-      child: AnimatedBuilder(
-        animation: scrollController,
-        builder: (context, child) {
-          double offset = scrollController.hasClients ? scrollController.offset : 0;
-          double parallaxOffset = offset * 0.1;
-          return Transform.translate(
-            offset: Offset(0, -parallaxOffset),
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: List.generate(
-                3,
-                (index) {
-                  return Container(
-                    height: 600,
-                    width: 300,
-                    color: Colors.grey[300],
-                    child: Center(child: Text('Project ${index + 1}')),
-                  );
-                },
+    return AnimatedBuilder(
+      animation: scrollController,
+      builder: (context, child) {
+        double offset = scrollController.hasClients ? scrollController.offset : 0;
+        double parallaxOffset = offset * 0.1;
+        return Stack(
+          children: [
+            Positioned.fill(
+              child: Transform.translate(
+                offset: Offset(0, -parallaxOffset * 1),
+                child: ParticleNetwork(
+                  touchActivation: true,
+                  particleCount: 60,
+                  maxSpeed: 1.5,
+                  maxSize: 1.5,
+                  lineDistance: 340,
+                  particleColor: Theme.of(context).brightness == Brightness.dark ? Colors.white : Colors.black,
+                  lineColor: Theme.of(context).brightness == Brightness.dark ? Colors.white54 : Colors.black26,
+                  touchColor: Theme.of(context).colorScheme.secondary,
+                ),
               ),
             ),
-          );
-        },
-      ),
+            SectionWrapper(
+              center: true,
+              child: Transform.translate(
+                offset: Offset(0, -parallaxOffset),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: List.generate(
+                    3,
+                    (index) {
+                      return Container(
+                        margin: EdgeInsets.all(32),
+                        height: 600,
+                        width: 300,
+                        color: Colors.black,
+                        child: Center(child: Text('Project ${index + 1}')),
+                      );
+                    },
+                  ),
+                ),
+              ),
+            ),
+          ],
+        );
+      },
     );
   }
 }
